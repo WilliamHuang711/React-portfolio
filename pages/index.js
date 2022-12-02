4028b5cadb4e393da225519c7953e87eb962e7a5
William Huang <hh921088586@berkeley.edu>
2022-12-01 21:28:31 -0800

    navbar and personal information

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
 import Head from 'next/head'
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import Image from "next/image";
+import personal from "../public/personal.png";
 
 
 export default function Home() {
@@ -10,9 +17,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">WilliamH</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="https://www.linkedin.com/in/wenfeng-huang-14369323a/"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              William Huang
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Geographer and cartographer
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+            Hi! My name is William Huang, I am a cartographer; 
+            a part-time hair colorist; and currently styduing in web development.
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillGithub />
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={personal} layout="fill" objectFit="cover" />
+            </div>
+          </div>
         </section>
 
       </main>
